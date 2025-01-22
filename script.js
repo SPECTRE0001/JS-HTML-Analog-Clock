0e82533ef504d8770a77ea861c073d67bef7fc95
SPECTRE0001 <117543012+SPECTRE0001@users.noreply.github.com>
2025-01-22 17:42:41 -0500

    Update script.js

--- script.js
+++ script.js
@@ -13,7 +13,8 @@ setInterval(
 //multiply by 6 to get degree amount (6*60=360)        
         second=d.getSeconds() * 6;
         minute=d.getMinutes() * 6;
-        hour=d.getHours() * 6;
+//accounts for the 12 hours at 30 degrees each and moves the hand by .5 degrees every second.
+        hour = (d.getHours() % 12) * 30 + d.getMinutes() * 0.5;
         document.getElementById("second-hand").style.transform = "rotate("+second+"deg)";
         document.getElementById("minute-hand").style.transform = "rotate("+minute+"deg)";
         document.getElementById("hour-hand").style.transform = "rotate("+hour+"deg)";
